74ec3064f45df96db3441719584fa757a6020483
atembamanu <atembaemm07@gmail.com>
2020-01-31 16:26:49 +0300

    Create a Preference activity

---
+++ app/src/main/java/com/blueman/ammusic/Activities/MusicPreferenceActivity.java
@@ -0,0 +1,16 @@
+package com.blueman.ammusic.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.blueman.ammusic.R;
+
+public class MusicPreferenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_preference);
+    }
+}
