b41cb009afc0d1beddeb64c1151acde20c5d61b8
atembamanu <atembaemm07@gmail.com>
2020-01-31 16:19:36 +0300

    create launcher slides with features

---
+++ app/src/main/java/com/blueman/ammusic/Adapters/SliderAdapter.java
@@ -0,0 +1,100 @@
+package com.blueman.ammusic.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.blueman.ammusic.R;
+
+import butterknife.BindView;
+
+public class SliderAdapter extends PagerAdapter {
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public SliderAdapter(Context context){
+        this.context = context;
+
+    }
+    //Arrays
+    private int[] slide_images = {
+            R.drawable.beats_logo,
+            R.drawable.voice,
+            R.drawable.read_lyrics
+
+    };
+    private String[] slide_headings = {
+            "",
+            "VOICE",
+            "LYRICS"
+
+
+    };
+    private String[] slide_descs_1 = {
+            "PLAY MUSIC",
+            "CONTROL PLAYLIST",
+            "SOMA LYRICS"
+
+
+    };
+    private String[] slide_descs_2 = {
+            "ON THE",
+            "USING YOUR",
+            "MUSIC IKI"
+
+
+    };
+    private String[] slide_descs_3 = {
+            "GO",
+            "VOICE",
+            "BEAT"
+
+
+    };
+    @Override
+    public int getCount() {
+        return slide_headings.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide_layout, container, false);
+
+        ImageView slideImageView = (ImageView) view.findViewById(R.id.image_view);
+        TextView slideHeadings = (TextView) view.findViewById(R.id.heading);
+        TextView slideDescription_1 = (TextView) view.findViewById(R.id.text_1);
+        TextView slideDescription_2 = (TextView) view.findViewById(R.id.text_2);
+        TextView slideDescription_3 = (TextView) view.findViewById(R.id.text_3);
+
+
+        slideImageView.setImageResource(slide_images[position]);
+        slideHeadings.setText(slide_headings[position]);
+        slideDescription_1.setText(slide_descs_1[position]);
+        slideDescription_2.setText(slide_descs_2[position]);
+        slideDescription_3.setText(slide_descs_3[position]);
+
+        container.addView(view);
+
+        return  view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((RelativeLayout) object);
+
+    }
+}
--- app/src/main/java/com/blueman/ammusic/MainActivity.java
+++ app/src/main/java/com/blueman/ammusic/MainActivity.java
@@ -1,14 +1,125 @@
 package com.blueman.ammusic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.blueman.ammusic.Adapters.SliderAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.slideview_pager) ViewPager mSlideViewPager;
+    @BindView(R.id.dotsLayout) LinearLayout mDotLayout;
+
+    @BindView(R.id.backBtn) Button mBackBtn;
+    @BindView(R.id.nextBtn) Button mNextBtn;
+
+    private TextView[] mDots;
+    private int mCurrentPage;
+
+    private SliderAdapter sliderAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        sliderAdapter = new SliderAdapter(this);
+        ButterKnife.bind(this);
+
+
+        mSlideViewPager.setAdapter(sliderAdapter);
+
+        addDotsIndicator(0);
+        mSlideViewPager.addOnPageChangeListener(viewListener);
+
+        //OnclickListeners for Buttons
+        mNextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSlideViewPager.setCurrentItem(mCurrentPage+1);
+            }
+        });
+
+        mBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSlideViewPager.setCurrentItem(mCurrentPage - 1);
+            }
+        });
     }
+
+
+    public void addDotsIndicator(int position){
+
+        mDots = new TextView[3];
+        mDotLayout.removeAllViews();
+        for (int i = 0; i < mDots.length; i++){
+            mDots[i] = new TextView(this);
+            mDots[i].setText(Html.fromHtml("&#8226"));
+            mDots[i].setTextSize(35);
+            mDots[i].setTextColor(getResources().getColor(R.color.colorTransparent));
+            mDotLayout.addView(mDots[i]);
+        }
+
+        if (mDots.length > 0){
+            mDots[position].setTextColor(getResources().getColor(R.color.colorPrimary));
+        }
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+            addDotsIndicator(position);
+            mCurrentPage =position;
+
+            if(position == 0){
+
+                mBackBtn.setEnabled(false);
+                mNextBtn.setEnabled(true);
+                mBackBtn.setVisibility(View.INVISIBLE);
+
+                mNextBtn.setText("Next");
+                mBackBtn.setText("");
+            }else if(position == mDots.length-1){
+
+                mBackBtn.setEnabled(true);
+                mNextBtn.setEnabled(true);
+                mBackBtn.setVisibility(View.VISIBLE);
+
+                mNextBtn.setText("Finish");
+                mBackBtn.setText("Back");
+            }else {
+
+                mBackBtn.setEnabled(true);
+                mNextBtn.setEnabled(true);
+                mBackBtn.setVisibility(View.VISIBLE);
+                mNextBtn.setVisibility(View.VISIBLE);
+
+
+                mNextBtn.setText("Next");
+                mBackBtn.setText("Back");
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 }
