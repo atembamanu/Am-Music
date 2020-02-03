5d3ef31d1925b9ef90e8531384d52f7d9078d9b4
atembamanu <atembaemm07@gmail.com>
2020-02-03 07:32:32 +0300

    Onclick toggle mContinue button visible

--- app/src/main/java/com/blueman/ammusic/Activities/HomeActivity.java
+++ app/src/main/java/com/blueman/ammusic/Activities/HomeActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.blueman.ammusic.R;
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/com/blueman/ammusic/Activities/MusicPreferenceActivity.java
+++ app/src/main/java/com/blueman/ammusic/Activities/MusicPreferenceActivity.java
@@ -33,12 +33,14 @@ public class MusicPreferenceActivity extends AppCompatActivity implements View.O
     @BindView(R.id.card_mPop) TextView mPop;
 
 
+    @BindView(R.id.mcontinue) TextView mContinue;
+
     boolean clicked = false;
     private final String LOG_TAG = MusicPreferenceActivity.class.getSimpleName();
 
     //private TextView[] musicPreferences = {mGospel, mReggae, mRumba, mRnB, mTechno, mSoul, mCountry, mHipHop, mJazz, mRock, mInstrumental,mPop };
    // private int[] ids = {R.id.card_mGospel, R.id.card_mReggae, R.id.card_mRumba, R.id.card_mRnB, R.id.card_mTechno,R.id.card_mSoul,R.id.card_mCountry,R.id.card_mHiphop,R.id.card_mJazz,R.id.card_mRock,R.id.card_mInstrumental,R.id.card_mPop};
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -69,64 +71,76 @@ public class MusicPreferenceActivity extends AppCompatActivity implements View.O
                 case R.id.card_mGospel:
                     mGospel.setBackgroundColor(getResources().getColor(R.color.colorWhite));
                     mGospel.setTextColor(getResources().getColor(R.color.colorPrimary));
+                    mContinue.setVisibility(View.VISIBLE);
                     clicked = true;
                     break;
 
                 case R.id.card_mReggae:
                     mReggae.setBackgroundColor(getResources().getColor(R.color.colorWhite));
                     mReggae.setTextColor(getResources().getColor(R.color.colorPrimary));
+                    mContinue.setVisibility(View.VISIBLE);
                     clicked = true;
                     break;
                 case R.id.card_mRumba:
                     mRumba.setBackgroundColor(getResources().getColor(R.color.colorWhite));
                     mRumba.setTextColor(getResources().getColor(R.color.colorPrimary));
+                    mContinue.setVisibility(View.VISIBLE);
                     clicked = true;
                     break;
                 case R.id.card_mRnB:
                     mRnB.setBackgroundColor(getResources().getColor(R.color.colorWhite));
                     mRnB.setTextColor(getResources().getColor(R.color.colorPrimary));
+                    mContinue.setVisibility(View.VISIBLE);
                     clicked = true;
                     break;
                 case R.id.card_mTechno:
                     mTechno.setBackgroundColor(getResources().getColor(R.color.colorWhite));
                     mTechno.setTextColor(getResources().getColor(R.color.colorPrimary));
+                    mContinue.setVisibility(View.VISIBLE);
                     clicked = true;
                     break;
 
                 case R.id.card_mSoul:
                     mSoul.setBackgroundColor(getResources().getColor(R.color.colorWhite));
                     mSoul.setTextColor(getResources().getColor(R.color.colorPrimary));
+                    mContinue.setVisibility(View.VISIBLE);
                     clicked = true;
                     break;
                 case R.id.card_mCountry:
                     mCountry.setBackgroundColor(getResources().getColor(R.color.colorWhite));
                     mCountry.setTextColor(getResources().getColor(R.color.colorPrimary));
+                    mContinue.setVisibility(View.VISIBLE);
                     clicked = true;
                     break;
                 case R.id.card_mHiphop:
                     mHipHop.setBackgroundColor(getResources().getColor(R.color.colorWhite));
                     mHipHop.setTextColor(getResources().getColor(R.color.colorPrimary));
+                    mContinue.setVisibility(View.VISIBLE);
                     clicked = true;
                     break;
                 case R.id.card_mJazz:
                     mJazz.setBackgroundColor(getResources().getColor(R.color.colorWhite));
                     mJazz.setTextColor(getResources().getColor(R.color.colorPrimary));
+                    mContinue.setVisibility(View.VISIBLE);
                     clicked = true;
                     break;
 
                 case R.id.card_mRock:
                     mRock.setBackgroundColor(getResources().getColor(R.color.colorWhite));
                     mRock.setTextColor(getResources().getColor(R.color.colorPrimary));
+                    mContinue.setVisibility(View.VISIBLE);
                     clicked = true;
                     break;
                 case R.id.card_mInstrumental:
                     mInstrumental.setBackgroundColor(getResources().getColor(R.color.colorWhite));
                     mInstrumental.setTextColor(getResources().getColor(R.color.colorPrimary));
+                    mContinue.setVisibility(View.VISIBLE);
                     clicked = true;
                     break;
                 case R.id.card_mPop:
                     mPop.setBackgroundColor(getResources().getColor(R.color.colorWhite));
                     mPop.setTextColor(getResources().getColor(R.color.colorPrimary));
+                    mContinue.setVisibility(View.VISIBLE);
                     clicked = true;
                     break;
                     default:
