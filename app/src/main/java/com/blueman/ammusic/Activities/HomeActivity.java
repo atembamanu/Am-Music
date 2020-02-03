5253d1b2e165ec770d462c794156f291e3ca1d4b
atembamanu <atembaemm07@gmail.com>
2020-02-03 10:59:02 +0300

    Get intent from prevous activity and display in textview

--- app/src/main/java/com/blueman/ammusic/Activities/HomeActivity.java
+++ app/src/main/java/com/blueman/ammusic/Activities/HomeActivity.java
@@ -1,16 +1,30 @@
 package com.blueman.ammusic.Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.blueman.ammusic.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private String user_BeatName;
+    @BindView(R.id.user_name)
+    TextView user_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+
+        user_BeatName = getIntent().getStringExtra("user_name");
+
+        user_name.setText("Welcome "+user_BeatName);
+
     }
 }
