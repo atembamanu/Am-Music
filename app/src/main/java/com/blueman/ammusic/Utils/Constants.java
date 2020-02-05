4859f4b8d8bf0efc84ff5ac471f6be765652b487
atembamanu <atembaemm07@gmail.com>
2020-02-05 12:51:17 +0300

    set up apikey and initiliaze it in Contants.java file

---
+++ app/src/main/java/com/blueman/ammusic/Utils/Constants.java
@@ -0,0 +1,9 @@
+package com.blueman.ammusic.Utils;
+
+import com.blueman.ammusic.BuildConfig;
+
+public class Constants {
+
+    public static final String BASE_URL = "https://api.musixmatch.com/ws/1.1/";
+    public static final  String API_KEY = BuildConfig.API_KEY;
+}
